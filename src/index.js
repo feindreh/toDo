364a8f6b5c5e37b1063d4c36548a41caac0ec0b0
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-24 03:33:22 +0100

    toDoFactory made sure dependencys work

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./style.css";
 import toDoFactory from "./toDoFactory";
 
-console.log(toDoFactory());
+
+//toDoFactory (title,description,dueDate,proxTime,priority,notes,checklist)
+const toDos = [];
+toDos.push(toDoFactory("top","toDo list fertig machen", "heute","2h",undefined,"mindestens die funktionen"));
+console.log(toDos);
--- src/toDoFactory.js
+++ src/toDoFactory.js
@@ -1,5 +1,5 @@
-const toDoFactory = () => {
-	return {};
+const toDoFactory = (title,description,dueDate,proxTime,priority = "0",notes,checklist) => {
+	return {title,description,dueDate,priority,proxTime,notes,checklist};
 };
 
 export default toDoFactory;
