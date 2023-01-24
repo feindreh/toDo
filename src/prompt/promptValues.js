af050f2200bc8ccbec0be62156391a63456d712f
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-24 05:44:51 +0100

    created value functions for closeButton

---
+++ src/prompt/promptValues.js
@@ -0,0 +1,12 @@
+export function getValues(inputContainer){
+	//take node return input values
+	console.log("get Values working");
+
+}
+
+export function resetValues(inputContainer){
+	//take node reset input values
+	console.log("reset Values working");
+
+}
+
