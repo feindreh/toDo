4c6b2054f9e31d26dcacb09c10b728330a9778ad
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-24 16:11:10 +0100

    add filler content for layout

--- src/contentDir/content.js
+++ src/contentDir/content.js
@@ -1,10 +1,13 @@
-import { divID } from "../functions/creation";
+import { divClass, divID } from "../functions/creation";
 
 export default function createContent(){
 
 	const content = divID("content");
+	const postit =  divID("PostIt");
+	const dummy1 = divClass("dummy");
+	const dummy2 = divClass("dummy");
 
-	content.append();
+	content.append(dummy1,postit,dummy2);
 
 	return content;
 }
