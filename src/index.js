c3725515ed29c98e6c692711390a0cfa7039fc01
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-24 04:42:58 +0100

    add button newButton to open a prompt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./style.css";
 import toDoFactory from "./toDoFactory";
+import openNewToDoPrompt  from "./prompt";
 
+const newButton = document.querySelector("#newButton");
+newButton.addEventListener(("click"), () => {
+	const event = openNewToDoPrompt();
+	console.log(event);
+});
---
+++ src/prompt.js
@@ -0,0 +1,3 @@
+export default function openNewToDoPrompt(){
+	return "a new prompt";
+}
