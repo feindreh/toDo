35de719fe6edde546e01afd2ba2a7c78bd5d8072
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-24 05:27:36 +0100

    better file and fucntion name

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./style.css";
 import toDoFactory from "./toDoFactory";
-import newToDoPrompt  from "./prompt/prompt";
+import createPrompt  from "./prompt/createPrompt";
 
 
 const container = document.querySelector("#container");
-container.append(newToDoPrompt());
+container.append(createPrompt());
 
 const newButton = document.querySelector("#newButton");
 const closeButton = document.querySelector("#closeButton");
---
+++ src/prompt/createPrompt.js
@@ -0,0 +1,30 @@
+import {divID,divClass} from "../functions/creation.js";
+
+// toDoFactory = (title,description,dueDate,proxTime,priority = "0") =>
+
+export default function createPrompt(){
+	//return the prompt as node
+
+
+	//create elements
+	const promptContainer = divID("newPrompt");
+
+	const input = document.createElement("input");
+	input.setAttribute("class","textField");
+
+	const closeButton = document.createElement("button");
+	closeButton.setAttribute("id","closeButton");
+	closeButton.innerText = "... finishing this to Do!";
+
+	//append elements to container
+
+	for(let i = 1; i<=4;i++){
+		let newNode = input.cloneNode(true);
+		promptContainer.append(newNode);
+	}
+
+	promptContainer.append(closeButton);
+	
+	//return the container
+	return promptContainer;
+}
