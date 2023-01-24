1a77fe5c2b2206433a987669bbe5279de2029a1a
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-24 06:33:22 +0100

    changed toDoFactory to take value object as input

--- src/toDoFactory.js
+++ src/toDoFactory.js
@@ -1,4 +1,9 @@
-const toDoFactory = (title,description,dueDate,proxTime,priority = "0") => {
+const toDoFactory = (values) => {
+	let title = values.title;
+	let description = values.description;
+	let dueDate = values.dueDate;
+	let priority = 0;
+	let proxTime = values.proxTime;
 	return {title,description,dueDate,priority,proxTime};
 };
 
