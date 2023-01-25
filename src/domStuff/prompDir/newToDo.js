19847ccc1287a8c07b88398d47656284100b1534
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-25 05:50:00 +0100

    add projectPrompt function + refactor

--- src/domStuff/prompDir/newToDo.js
+++ src/domStuff/prompDir/newToDo.js
@@ -1,35 +1,35 @@
 import {divID,divClass} from "../creation.js";
 import createCloseButton from "../prompDir/closeButton";
 
+function createInput(name){
+	let newInput = document.createElement("input");
+	newInput.setAttribute("class","textField");
+	newInput.setAttribute("id",`${name}`);
+	return newInput;
+}
+function createLabel(name){
+	let newLabel = document.createElement("label");
+	newLabel.setAttribute("class", "textLabel");
+	newLabel.innerText = `${name}`;
+	return newLabel;
+}
 
-export default function createPrompt(){
+function createInputBlock(name){
+	let input = createInput(name);
+	let label = createLabel(name);
+	let block = divClass("inputWrap");
+	block.append(label,input);
+	return block;
+}
+
+
+export function createToDoPrompt(){
 	//return the prompt as node
 
 
 	//create elements
 	
-	function createInput(name){
-		let newInput = document.createElement("input");
-		newInput.setAttribute("class","textField");
-		newInput.setAttribute("id",`${name}`);
-		return newInput;
-	}
-	function createLabel(name){
-		let newLabel = document.createElement("label");
-		newLabel.setAttribute("class", "textLabel");
-		newLabel.innerText = `${name}`;
-		return newLabel;
-	}
-
-	function createInputBlock(name){
-		let input = createInput(name);
-		let label = createLabel(name);
-		let block = divClass("inputWrap");
-		block.append(label,input);
-		return block;
-	}
-
-	const promptContainer = divID("newPrompt");
+	const promptContainer = divID("toDoPrompt");
 
 	const title = createInputBlock("title");
 	const description = createInputBlock("description");
@@ -42,6 +42,25 @@ export default function createPrompt(){
 
 	promptContainer.append(title,description,dueDate,proxTime,closeButton);
 	
+	
+	return promptContainer;
+}
+
+export function createProjectPrompt(){
+	//return the prompt as node
+
+
+	//create elements
+
+	const promptContainer = divID("projectPrompt");
+
+	const title = createInputBlock("title");
+	const description = createInputBlock("description");
+	const dueDate = createInputBlock("dueDate");
+	const proxTime = createInputBlock("proxTime");
+
+	//append elements to container
+	promptContainer.append(title,description,dueDate,proxTime);
 	//return the container
 	return promptContainer;
 }
