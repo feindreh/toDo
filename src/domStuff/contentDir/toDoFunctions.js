fcfe266c1df711a8c910bfefc78f823c8f3ef5c2
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-25 07:44:00 +0100

    betterstructure

--- src/domStuff/contentDir/toDoFunctions.js
+++ src/domStuff/contentDir/toDoFunctions.js
@@ -1,41 +1,6 @@
-import { divClass} from "../creation";
-import { createNewToDoButton } from "../headerDir/ButtonCreation";
+import { createToDoElement } from "./postItCreation";
 
-export function createToDoElement(toDo){
-	//create Element with the struktur of the toDoElement
-    
 
-	const toDoElement = divClass(`toDoContainer-${toDo.type}`);
-	toDoElement.addEventListener("click",()=>{
-		console.log("container");
-		console.log("toDos");
-	});
-
-	const toDoMark = divClass("toDoMark");
-	toDoMark.innerText = "To Do:";
-
-	const header = divClass("toDo-Header");
-	header.innerText = `${toDo.title}`;
-
-	const description = divClass("toDo-Description");
-	description.innerText = `${toDo.description}`;
-
-	const proxTimeP = document.createElement("div");
-	proxTimeP.innerText = "aproximate time to finish";
-
-	const proxTime = divClass("toDo-ProxTime");
-	proxTime.innerText = `${toDo.proxTime}`;
-
-	const dueDateP = document.createElement("div");
-	dueDateP.innerText = "Should be done till";
-
-	const dueDate = divClass("toDo-dueDate");
-	dueDate.innerText = `${toDo.dueDate}`;
-
-	toDoElement.append(toDoMark,header,description,proxTimeP,proxTime,dueDateP,dueDate);
-	
-	return toDoElement;
-}
 
 export function displayToDos(toDos,node){
 	// display toDos[array] => node
