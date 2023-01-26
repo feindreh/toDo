c0e0da4cecc2bef514c488a24749eef1a50be1dd
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-26 09:14:37 +0100

    changed path => dummy

--- src/elementCreationDir/content.js
+++ src/elementCreationDir/content.js
@@ -3,11 +3,11 @@ import { divClass, divID } from "./creation";
 export default function createContent(){
 
 	const content = divID("content");
-	const postit =  divID("PostIt");
+	const dummy1 =  divClass("dummy");
 	const path = divID("path");
 	const dummy2 = divClass("dummy");
 
-	content.append(path,postit,dummy2);
+	content.append(dummy1,postit,dummy2);
 
 	return content;
 }
